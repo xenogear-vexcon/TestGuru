cd43a7d52c07558e68a8567ce92c1e2f91efc554
Xenogear <celevra@rambler.ru>
2019-05-02 18:27:48 +0300

    Part_5 refact

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author
+  belongs_to :author, class_name: "User"
   has_many :questions, dependent: :destroy
   has_many :tests_users
   has_many :users, through: :tests_users
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
   has_many :results, dependent: :destroy
-  has_many :authorship, foreign_key: "author_id"
+  has_many :authorship, class_name: "Test", foreign_key: "author_id", dependent: :nullify
   
   def tests_by_level(test_level)
     Test.join('JOIN results ON results.test_id = tests.id')
