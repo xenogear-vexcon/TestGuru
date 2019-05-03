56db3e9125a8f8f585100ce2188a8818bb2bd2c8
Xenogear <celevra@rambler.ru>
2019-05-03 12:42:41 +0300

    Part_5_refact

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :questions, dependent: :destroy
-  has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results
 
   def self.sort_by_category(test_category)
-    Test.joins("JOIN categories ON categories.id = tests.category_id")
-        .where(categories: { title: test_category })
-        .order(title: :desc).pluck(:title)
+    joins(:category).where(categories: { title: test_category })
+                    .order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ApplicationRecord
-  has_many :answers, dependent: :destroy
-  has_many :tests_users
-  has_many :tests, through: :tests_users
   has_many :results, dependent: :destroy
+  has_many :tests, through: :results
   has_many :authorship, class_name: "Test", foreign_key: "author_id", dependent: :nullify
   
   def tests_by_level(test_level)
-    Test.join('JOIN results ON results.test_id = tests.id')
-        .where(level: test_level, results: { user_id: id })
+    tests.where(level: test_level)
   end
 
 end
