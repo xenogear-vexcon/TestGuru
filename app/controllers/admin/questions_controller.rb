5d1a1ff8ce4c345cfbcb1a400f3316f1fbc0f368
Xenogear <celevra@rambler.ru>
2019-07-01 20:52:47 +0300

    Part_13

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -16,7 +16,7 @@ class Admin::QuestionsController < Admin::BaseController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to [:admin, @question]
+      redirect_to admin_question_path(@question)
     else
       render 'new'
     end
@@ -25,7 +25,7 @@ class Admin::QuestionsController < Admin::BaseController
   def update
 
     if @question.update(question_params)
-      redirect_to [:admin, @question]
+      redirect_to admin_question_path(@question)
     else
       render 'edit'
     end
@@ -34,7 +34,7 @@ class Admin::QuestionsController < Admin::BaseController
   def destroy
 
     @question.destroy
-    redirect_to [:admin, @question.test]
+    redirect_to admin_test_path(@question.test)
   end
 
   private
