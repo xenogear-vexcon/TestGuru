1be2d619a039f3c7f1f618a22869051e1cfff02a
Xenogear <celevra@rambler.ru>
2019-07-07 12:18:39 +0300

    Part_14

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,7 +1,7 @@
 class TestPassagesController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :find_test_passage, only: %i[show update result]
+  before_action :find_test_passage, only: %i[show update result gist]
 
   def show; end
 
@@ -18,10 +18,24 @@ class TestPassagesController < ApplicationController
     end
   end
 
+  def gist
+    result = GistQuestionService.new(@test_passage.current_question).call
+
+    flash_options = if result.success?
+      create_gist!(gist_url)
+      { notice: t('.success', url: gist_url.html_url) }
+    else
+      { alert: t('.failure') }
+    end
+
   private
 
   def find_test_passage
     @test_passage = TestPassage.find(params[:id])
+  end
+
+  def create_gist!(gist_url)
+    current_user.gists.create(user: @test_passage.user, question: @test_passage.current_question, url: gist_url.html_url)
   end
 
 end
---
+++ app/models/gist.rb
@@ -0,0 +1,8 @@
+class Gist < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :question
+
+  validates :url, presence: true
+  
+end
