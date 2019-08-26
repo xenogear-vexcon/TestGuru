257b4d51b9a3b131f5096ba2c957dcb95b8c6192
Xenogear <celevra@rambler.ru>
2019-08-26 14:00:10 +0300

    refact

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
