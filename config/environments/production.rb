033c444c1ae40c7d454670b8c627a1a6add8ab0b
Xenogear <celevra@rambler.ru>
2019-08-27 16:03:04 +0300

    refact

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,6 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.read_encrypted_secrets = true
-  
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
