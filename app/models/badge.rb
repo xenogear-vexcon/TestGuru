3460e623f5fb0b4c0a9eca24a1655e7ed630fdd5
Xenogear <celevra@rambler.ru>
2020-01-20 00:27:09 +0300

    update badge_service

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -7,7 +7,8 @@ class Badge < ApplicationRecord
 
   validates :title, uniqueness: true, inclusion: { in: BadgeService.badge_titles.map{|title| "#{title}"}, message: "%{value} is not a valid title" }
   validates :rule, inclusion: { in: BadgeService.badge_rules.map{|rule| "#{rule}"}, message: "%{value} is not a valid rule" }
-  validates :option, uniqueness: { scope: :rule, message: "This combination of option and rule already exists." }
+  validates :option, uniqueness: { scope: :rule, message: "This combination of option and rule already exists." },
+                     inclusion: { in: BadgeService.option.map{|option| "#{option}"}, message: "%{value} is not a valid option" }
   validates :image, attached: true, content_type: [:png, :jpg, :jpeg]
 
 end
--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -10,21 +10,9 @@ class BadgeService
     Badge.all.select { |badge| send("#{badge.rule}?", badge.option, badge.id) }
   end
 
+
+
   private
-
-  def check_badge_rule(rule, badge_id)
-    return unless @test_passage.success? && rule.to_i == @test.rule
-
-    tests_by_rule = Test.where(rule: rule.to_i)
-    user_tests_by_rule = @user.test_passages.success.where(rule: tests_by_rule)
-
-    if @user.user_badges.where(badge_id: badge_id.to_i).any?
-      last_badge_date = @user.user_badges.where(badge_id: badge_id.to_i).order(created_at: :asc).last.created_at
-      user_tests_by_rule = user_tests_by_rule.where("created_at > ?", last_badge_date)
-    end
-
-    tests_by_rule.pluck(:id).count == user_tests_by_rule.pluck(:id).uniq.count
-  end
 
   def first_attempt?(option, badge_id)
     return unless @test_passage.success?
@@ -32,12 +20,32 @@ class BadgeService
     TestPassage.where(user_id: @user.id, test_id: @test.id).success.count == 1
   end
 
-  def tests_by_level_finished?(level, badge_id)
-    check_badge_rule(level, badge_id)
+  def tests_by_category_finished?(category_id, badge_id)
+    return unless @test_passage.success? && category_id == @test.category_id
+
+    tests_by_rule = Test.where(category_id: category_id).ids
+    user_tests_by_rule = @user.test_passages.success.where(test_id: tests_by_rule)
+
+    if @user.user_badges.where(badge_id: badge_id.to_i).any?
+      last_badge_date = @user.user_badges.where(badge_id: badge_id.to_i).order(created_at: :asc).last.created_at
+      user_tests_by_rule = user_tests_by_rule.where("created_at > ?", last_badge_date)
+    end
+
+    tests_by_rule.count == user_tests_by_rule.pluck(:test_id).uniq.count
   end
 
-  def tests_by_category_finished?(category_id, badge_id)
-    check_badge_rule(category_id, badge_id)
+  def tests_by_level_finished?(level, badge_id)
+    return unless @test_passage.success? && level == @test.level
+
+    tests_by_rule = Test.where(level: level).ids
+    user_tests_by_rule = @user.test_passages.success.where(test_id: tests_by_rule)
+
+    if @user.user_badges.where(badge_id: badge_id.to_i).any?
+      last_badge_date = @user.user_badges.where(badge_id: badge_id.to_i).order(created_at: :asc).last.created_at
+      user_tests_by_rule = user_tests_by_rule.where("created_at > ?", last_badge_date)
+    end
+
+    tests_by_rule.count == user_tests_by_rule.pluck(:test_id).uniq.count
   end
 
 
@@ -49,6 +57,10 @@ class BadgeService
     [ :first_try, :first_try_100,
       :category, :category_100,
       :level, :level_100 ]
+  end
+
+  def self.option
+    [ :option, :level, :category ]
   end
 
 end
